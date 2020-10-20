c258e20627211585e56415c5fd749f8e162cb804
Jonathan Chan <jonchan3990@gmail.com>
2020-10-19 21:30:07 -0700

    Initial commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+};
+
+export default App;
